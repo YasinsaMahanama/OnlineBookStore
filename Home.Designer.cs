76281d90c4acebf68f7201b2c4aa897000a234fa
Yasinsa Mahanama <yasinsamahanama@gmail.com>
2024-05-21 00:26:38 +0530

    Home page udpated

--- Home.Designer.cs
+++ Home.Designer.cs
@@ -34,16 +34,16 @@
             pictureBox1 = new PictureBox();
             panel2 = new Panel();
             pictureBox2 = new PictureBox();
-            button2 = new Button();
+            Login = new Button();
             button1 = new Button();
             textBox1 = new TextBox();
             panel3 = new Panel();
-            button6 = new Button();
-            button5 = new Button();
-            button4 = new Button();
-            button9 = new Button();
-            button11 = new Button();
-            button3 = new Button();
+            ContactBtn = new Button();
+            AboutBtn = new Button();
+            UserBtn = new Button();
+            InStockBtn = new Button();
+            BookBtn = new Button();
+            SaleBtn = new Button();
             pictureBox3 = new PictureBox();
             panel4 = new Panel();
             label5 = new Label();
@@ -91,7 +91,7 @@
             // 
             panel2.BackColor = Color.FromArgb(0, 0, 64);
             panel2.Controls.Add(pictureBox2);
-            panel2.Controls.Add(button2);
+            panel2.Controls.Add(Login);
             panel2.Controls.Add(button1);
             panel2.Controls.Add(textBox1);
             panel2.Controls.Add(pictureBox1);
@@ -111,15 +111,16 @@
             pictureBox2.TabIndex = 7;
             pictureBox2.TabStop = false;
             // 
-            // button2
+            // Login
             // 
-            button2.BackColor = Color.White;
-            button2.Location = new Point(1026, 21);
-            button2.Name = "button2";
-            button2.Size = new Size(147, 43);
-            button2.TabIndex = 6;
-            button2.Text = "Login / Register";
-            button2.UseVisualStyleBackColor = false;
+            Login.BackColor = Color.White;
+            Login.Location = new Point(1026, 21);
+            Login.Name = "Login";
+            Login.Size = new Size(147, 43);
+            Login.TabIndex = 6;
+            Login.Text = "Login / Register";
+            Login.UseVisualStyleBackColor = false;
+            Login.Click += Login_Click;
             // 
             // button1
             // 
@@ -145,95 +146,101 @@
             // panel3
             // 
             panel3.BackColor = Color.White;
-            panel3.Controls.Add(button6);
-            panel3.Controls.Add(button5);
-            panel3.Controls.Add(button4);
-            panel3.Controls.Add(button9);
-            panel3.Controls.Add(button11);
-            panel3.Controls.Add(button3);
+            panel3.Controls.Add(ContactBtn);
+            panel3.Controls.Add(AboutBtn);
+            panel3.Controls.Add(UserBtn);
+            panel3.Controls.Add(InStockBtn);
+            panel3.Controls.Add(BookBtn);
+            panel3.Controls.Add(SaleBtn);
             panel3.Location = new Point(0, 162);
             panel3.Name = "panel3";
             panel3.Size = new Size(1280, 50);
             panel3.TabIndex = 4;
             // 
-            // button6
+            // ContactBtn
             // 
-            button6.BackColor = Color.Teal;
-            button6.FlatStyle = FlatStyle.Popup;
-            button6.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button6.Location = new Point(1100, 3);
-            button6.Name = "button6";
-            button6.Size = new Size(141, 44);
-            button6.TabIndex = 12;
-            button6.Text = "CONTACT";
-            button6.UseVisualStyleBackColor = false;
+            ContactBtn.BackColor = Color.Teal;
+            ContactBtn.FlatStyle = FlatStyle.Popup;
+            ContactBtn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            ContactBtn.Location = new Point(1100, 3);
+            ContactBtn.Name = "ContactBtn";
+            ContactBtn.Size = new Size(141, 44);
+            ContactBtn.TabIndex = 12;
+            ContactBtn.Text = "CONTACT";
+            ContactBtn.UseVisualStyleBackColor = false;
+            ContactBtn.Click += ContactBtn_Click;
             // 
-            // button5
+            // AboutBtn
             // 
-            button5.BackColor = Color.Teal;
-            button5.FlatStyle = FlatStyle.Popup;
-            button5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button5.Location = new Point(888, 3);
-            button5.Name = "button5";
-            button5.Size = new Size(141, 44);
-            button5.TabIndex = 11;
-            button5.Text = "ABOUT";
-            button5.UseVisualStyleBackColor = false;
+            AboutBtn.BackColor = Color.Teal;
+            AboutBtn.FlatStyle = FlatStyle.Popup;
+            AboutBtn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            AboutBtn.Location = new Point(888, 3);
+            AboutBtn.Name = "AboutBtn";
+            AboutBtn.Size = new Size(141, 44);
+            AboutBtn.TabIndex = 11;
+            AboutBtn.Text = "ABOUT";
+            AboutBtn.UseVisualStyleBackColor = false;
+            AboutBtn.Click += AboutBtn_Click;
             // 
-            // button4
+            // UserBtn
             // 
-            button4.BackColor = Color.Teal;
-            button4.FlatStyle = FlatStyle.Popup;
-            button4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button4.Location = new Point(684, 3);
-            button4.Name = "button4";
-            button4.Size = new Size(141, 44);
-            button4.TabIndex = 10;
-            button4.Text = "USERS";
-            button4.UseVisualStyleBackColor = false;
+            UserBtn.BackColor = Color.Teal;
+            UserBtn.FlatStyle = FlatStyle.Popup;
+            UserBtn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            UserBtn.Location = new Point(253, 3);
+            UserBtn.Name = "UserBtn";
+            UserBtn.Size = new Size(141, 44);
+            UserBtn.TabIndex = 10;
+            UserBtn.Text = "USERS";
+            UserBtn.UseVisualStyleBackColor = false;
+            UserBtn.Click += UserBtn_Click;
             // 
-            // button9
+            // InStockBtn
             // 
-            button9.BackColor = Color.Teal;
-            button9.FlatStyle = FlatStyle.Popup;
-            button9.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button9.Location = new Point(473, 3);
-            button9.Name = "button9";
-            button9.Size = new Size(141, 44);
-            button9.TabIndex = 9;
-            button9.Text = "IN STOCK";
-            button9.UseVisualStyleBackColor = false;
+            InStockBtn.BackColor = Color.Teal;
+            InStockBtn.FlatStyle = FlatStyle.Popup;
+            InStockBtn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            InStockBtn.Location = new Point(464, 3);
+            InStockBtn.Name = "InStockBtn";
+            InStockBtn.Size = new Size(141, 44);
+            InStockBtn.TabIndex = 9;
+            InStockBtn.Text = "IN STOCK";
+            InStockBtn.UseVisualStyleBackColor = false;
+            InStockBtn.Click += InStockBtn_Click;
             // 
-            // button11
+            // BookBtn
             // 
-            button11.BackColor = Color.Teal;
-            button11.FlatStyle = FlatStyle.Popup;
-            button11.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button11.Location = new Point(263, 3);
-            button11.Name = "button11";
-            button11.Size = new Size(141, 44);
-            button11.TabIndex = 8;
-            button11.Text = "NEW BOOKS";
-            button11.UseVisualStyleBackColor = false;
+            BookBtn.BackColor = Color.Teal;
+            BookBtn.FlatStyle = FlatStyle.Popup;
+            BookBtn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            BookBtn.Location = new Point(45, 3);
+            BookBtn.Name = "BookBtn";
+            BookBtn.Size = new Size(141, 44);
+            BookBtn.TabIndex = 8;
+            BookBtn.Text = "NEW BOOKS";
+            BookBtn.UseVisualStyleBackColor = false;
+            BookBtn.Click += BookBtn_Click;
             // 
-            // button3
+            // SaleBtn
             // 
-            button3.BackColor = Color.Teal;
-            button3.FlatStyle = FlatStyle.Popup;
-            button3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button3.Location = new Point(45, 3);
-            button3.Name = "button3";
-            button3.Size = new Size(150, 44);
-            button3.TabIndex = 0;
-            button3.Text = "SALE";
-            button3.UseVisualStyleBackColor = false;
+            SaleBtn.BackColor = Color.Teal;
+            SaleBtn.FlatStyle = FlatStyle.Popup;
+            SaleBtn.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            SaleBtn.Location = new Point(675, 3);
+            SaleBtn.Name = "SaleBtn";
+            SaleBtn.Size = new Size(150, 44);
+            SaleBtn.TabIndex = 0;
+            SaleBtn.Text = "SALE";
+            SaleBtn.UseVisualStyleBackColor = false;
+            SaleBtn.Click += SaleBtn_Click;
             // 
             // pictureBox3
             // 
             pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
-            pictureBox3.Location = new Point(9, 237);
+            pictureBox3.Location = new Point(45, 234);
             pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(605, 451);
+            pictureBox3.Size = new Size(578, 451);
             pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox3.TabIndex = 5;
             pictureBox3.TabStop = false;
@@ -246,16 +253,16 @@
             panel4.Controls.Add(label2);
             panel4.Controls.Add(label1);
             panel4.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            panel4.Location = new Point(620, 237);
+            panel4.Location = new Point(621, 234);
             panel4.Name = "panel4";
-            panel4.Size = new Size(648, 451);
+            panel4.Size = new Size(620, 451);
             panel4.TabIndex = 6;
             // 
             // label5
             // 
             label5.AutoSize = true;
             label5.ForeColor = SystemColors.ControlDarkDark;
-            label5.Location = new Point(182, 317);
+            label5.Location = new Point(183, 328);
             label5.Name = "label5";
             label5.Size = new Size(255, 31);
             label5.TabIndex = 5;
@@ -265,7 +272,7 @@
             // 
             label4.AutoSize = true;
             label4.ForeColor = SystemColors.ControlDarkDark;
-            label4.Location = new Point(70, 286);
+            label4.Location = new Point(63, 286);
             label4.Name = "label4";
             label4.Size = new Size(512, 31);
             label4.TabIndex = 4;
@@ -275,7 +282,7 @@
             // 
             label3.AutoSize = true;
             label3.ForeColor = SystemColors.ControlDarkDark;
-            label3.Location = new Point(93, 255);
+            label3.Location = new Point(84, 240);
             label3.Name = "label3";
             label3.Size = new Size(452, 31);
             label3.TabIndex = 3;
@@ -286,7 +293,7 @@
             // 
             label2.AutoSize = true;
             label2.ForeColor = SystemColors.ControlDarkDark;
-            label2.Location = new Point(20, 224);
+            label2.Location = new Point(3, 198);
             label2.Name = "label2";
             label2.Size = new Size(612, 31);
             label2.TabIndex = 2;
@@ -295,12 +302,13 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 48F, FontStyle.Bold);
-            label1.Location = new Point(14, 46);
+            label1.Font = new Font("Segoe UI", 46.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(8, 21);
             label1.Name = "label1";
-            label1.Size = new Size(631, 106);
+            label1.Size = new Size(606, 102);
             label1.TabIndex = 1;
             label1.Text = "NEW ARRIVALS";
+            label1.Click += label1_Click;
             // 
             // panel5
             // 
@@ -346,14 +354,14 @@
         private Button button1;
         private TextBox textBox1;
         private PictureBox pictureBox2;
-        private Button button2;
+        private Button Login;
         private Panel panel3;
-        private Button button11;
-        private Button button3;
-        private Button button6;
-        private Button button5;
-        private Button button4;
-        private Button button9;
+        private Button BookBtn;
+        private Button SaleBtn;
+        private Button ContactBtn;
+        private Button AboutBtn;
+        private Button UserBtn;
+        private Button InStockBtn;
         private PictureBox pictureBox3;
         private Panel panel4;
         private Label label1;
--- Home.cs
+++ Home.cs
@@ -11,5 +11,94 @@ namespace OnlineBookShop
         {
 
         }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the Login form
+            Login loginForm = new Login();
+
+            // Show the Login form
+            loginForm.Show();
+
+            // Hide the current Home form
+            this.Hide();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SaleBtn_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the Sale form
+            Sale saleForm = new Sale();
+
+            // Show the Sale form
+            saleForm.Show();
+
+            // Hide the current Home form
+            this.Hide();
+        }
+
+        private void BookBtn_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the Book form
+            Book bookForm = new Book();
+
+            // Show the Book form
+            bookForm.Show();
+
+            // Hide the current Home form
+            this.Hide();
+        }
+
+        private void InStockBtn_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the In Stock form
+            InStock inStockForm = new InStock();
+
+            // Show the In Stock form
+            inStockForm.Show();
+
+            // Hide the current Home form
+            this.Hide();
+        }
+
+        private void UserBtn_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the User form
+            User userForm = new User();
+
+            // Show the User form
+            userForm.Show();
+
+            // Hide the current Home form
+            this.Hide();
+        }
+
+        private void AboutBtn_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the About form
+            About aboutForm = new About();
+
+            // Show the About form
+            aboutForm.Show();
+
+            // Hide the current Home form
+            this.Hide();
+        }
+
+        private void ContactBtn_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the Contact form
+            Contact contactForm = new Contact();
+
+            // Show the About form
+            contactForm.Show();
+
+            // Hide the current Home form
+            this.Hide();
+        }
     }
 }
