94dfcbc841d8edeff325acf3816d9ddb33fafb39
Yasinsa Mahanama <yasinsamahanama@gmail.com>
2024-05-22 10:16:00 +0530

    books page updated

--- Book.Designer.cs
+++ Book.Designer.cs
@@ -28,20 +28,454 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Book));
+            panel1 = new Panel();
+            button6 = new Button();
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            label9 = new Label();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            panel2 = new Panel();
+            label4 = new Label();
+            Refreshbtn = new Button();
+            CatCbAearch = new ComboBox();
+            dataGridView1 = new DataGridView();
+            EditBtn = new Button();
+            Deletebtn = new Button();
+            ResetBtn = new Button();
+            Savebtn = new Button();
+            PriceTb = new TextBox();
+            label7 = new Label();
+            QtyTb = new TextBox();
+            label5 = new Label();
+            BCatCb = new ComboBox();
+            label2 = new Label();
+            button7 = new Button();
+            label8 = new Label();
+            BAuthorTb = new TextBox();
+            BTitleTb = new TextBox();
+            label3 = new Label();
+            label1 = new Label();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(0, 0, 64);
+            panel1.Controls.Add(button6);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label9);
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(314, 696);
+            panel1.TabIndex = 2;
+            // 
+            // button6
+            // 
+            button6.BackColor = SystemColors.ControlDarkDark;
+            button6.Font = new Font("Garamond", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button6.ForeColor = Color.Black;
+            button6.Location = new Point(59, 329);
+            button6.Name = "button6";
+            button6.Size = new Size(193, 45);
+            button6.TabIndex = 20;
+            button6.Text = "IN STOCK";
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ControlDarkDark;
+            button1.Font = new Font("Garamond", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(59, 498);
+            button1.Name = "button1";
+            button1.Size = new Size(193, 45);
+            button1.TabIndex = 19;
+            button1.Text = "ABOUT";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(-15, -10);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(173, 140);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 18;
+            pictureBox1.TabStop = false;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = SystemColors.ActiveCaptionText;
+            label9.BorderStyle = BorderStyle.FixedSingle;
+            label9.Font = new Font("Gabriola", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.ForeColor = Color.Turquoise;
+            label9.Location = new Point(164, 30);
+            label9.Name = "label9";
+            label9.Size = new Size(125, 53);
+            label9.TabIndex = 13;
+            label9.Text = "Book Shop";
+            label9.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.ControlDarkDark;
+            button5.Font = new Font("Garamond", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button5.ForeColor = Color.Black;
+            button5.Location = new Point(59, 582);
+            button5.Name = "button5";
+            button5.Size = new Size(193, 45);
+            button5.TabIndex = 17;
+            button5.Text = "CONTACT";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.ControlDarkDark;
+            button4.Font = new Font("Garamond", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.ForeColor = Color.Black;
+            button4.Location = new Point(59, 413);
+            button4.Name = "button4";
+            button4.Size = new Size(193, 45);
+            button4.TabIndex = 16;
+            button4.Text = "SALE";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ControlDarkDark;
+            button3.Font = new Font("Garamond", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.ForeColor = Color.Black;
+            button3.Location = new Point(59, 246);
+            button3.Name = "button3";
+            button3.Size = new Size(193, 45);
+            button3.TabIndex = 15;
+            button3.Text = "USERS";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Teal;
+            button2.FlatStyle = FlatStyle.Popup;
+            button2.Font = new Font("Garamond", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.Black;
+            button2.Location = new Point(59, 163);
+            button2.Name = "button2";
+            button2.Size = new Size(193, 45);
+            button2.TabIndex = 14;
+            button2.Text = "BOOKS";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.White;
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(Refreshbtn);
+            panel2.Controls.Add(CatCbAearch);
+            panel2.Controls.Add(dataGridView1);
+            panel2.Controls.Add(EditBtn);
+            panel2.Controls.Add(Deletebtn);
+            panel2.Controls.Add(ResetBtn);
+            panel2.Controls.Add(Savebtn);
+            panel2.Controls.Add(PriceTb);
+            panel2.Controls.Add(label7);
+            panel2.Controls.Add(QtyTb);
+            panel2.Controls.Add(label5);
+            panel2.Controls.Add(BCatCb);
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(button7);
+            panel2.Controls.Add(label8);
+            panel2.Controls.Add(BAuthorTb);
+            panel2.Controls.Add(BTitleTb);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(label1);
+            panel2.Font = new Font("Garamond", 16.2F, FontStyle.Bold);
+            panel2.ForeColor = Color.White;
+            panel2.Location = new Point(320, 12);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(948, 696);
+            panel2.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.ForeColor = Color.DarkBlue;
+            label4.Location = new Point(407, 289);
+            label4.Name = "label4";
+            label4.Size = new Size(132, 31);
+            label4.TabIndex = 32;
+            label4.Text = "Book List";
+            // 
+            // Refreshbtn
+            // 
+            Refreshbtn.BackColor = Color.Indigo;
+            Refreshbtn.Font = new Font("Garamond", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Refreshbtn.ForeColor = Color.White;
+            Refreshbtn.Location = new Point(492, 336);
+            Refreshbtn.Name = "Refreshbtn";
+            Refreshbtn.Size = new Size(93, 36);
+            Refreshbtn.TabIndex = 31;
+            Refreshbtn.Text = "Refresh";
+            Refreshbtn.UseVisualStyleBackColor = false;
+            // 
+            // CatCbAearch
+            // 
+            CatCbAearch.Font = new Font("Calibri", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            CatCbAearch.ForeColor = SystemColors.WindowFrame;
+            CatCbAearch.FormattingEnabled = true;
+            CatCbAearch.Items.AddRange(new object[] { "Programming", "Networkng", "Maths", "Physics", "Bio", "English" });
+            CatCbAearch.Location = new Point(298, 343);
+            CatCbAearch.Name = "CatCbAearch";
+            CatCbAearch.Size = new Size(151, 26);
+            CatCbAearch.TabIndex = 30;
+            CatCbAearch.Text = "Filter Category";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = Color.White;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 396);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(927, 271);
+            dataGridView1.TabIndex = 28;
+            // 
+            // EditBtn
+            // 
+            EditBtn.BackColor = Color.Indigo;
+            EditBtn.Font = new Font("Garamond", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            EditBtn.ForeColor = Color.White;
+            EditBtn.Location = new Point(492, 178);
+            EditBtn.Name = "EditBtn";
+            EditBtn.Size = new Size(110, 45);
+            EditBtn.TabIndex = 27;
+            EditBtn.Text = "Edit";
+            EditBtn.UseVisualStyleBackColor = false;
+            // 
+            // Deletebtn
+            // 
+            Deletebtn.BackColor = Color.Indigo;
+            Deletebtn.Font = new Font("Garamond", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Deletebtn.ForeColor = Color.White;
+            Deletebtn.Location = new Point(143, 178);
+            Deletebtn.Name = "Deletebtn";
+            Deletebtn.Size = new Size(110, 45);
+            Deletebtn.TabIndex = 26;
+            Deletebtn.Text = "Delete";
+            Deletebtn.UseVisualStyleBackColor = false;
+            // 
+            // ResetBtn
+            // 
+            ResetBtn.BackColor = Color.Indigo;
+            ResetBtn.Font = new Font("Garamond", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            ResetBtn.ForeColor = Color.White;
+            ResetBtn.Location = new Point(322, 178);
+            ResetBtn.Name = "ResetBtn";
+            ResetBtn.Size = new Size(110, 45);
+            ResetBtn.TabIndex = 25;
+            ResetBtn.Text = "Reset";
+            ResetBtn.UseVisualStyleBackColor = false;
+            // 
+            // Savebtn
+            // 
+            Savebtn.BackColor = Color.Indigo;
+            Savebtn.Font = new Font("Garamond", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Savebtn.ForeColor = Color.White;
+            Savebtn.Location = new Point(672, 178);
+            Savebtn.Name = "Savebtn";
+            Savebtn.Size = new Size(110, 45);
+            Savebtn.TabIndex = 24;
+            Savebtn.Text = "Save";
+            Savebtn.UseVisualStyleBackColor = false;
+            // 
+            // PriceTb
+            // 
+            PriceTb.Location = new Point(731, 117);
+            PriceTb.Name = "PriceTb";
+            PriceTb.Size = new Size(180, 38);
+            PriceTb.TabIndex = 23;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Calibri", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.Black;
+            label7.Location = new Point(731, 86);
+            label7.Name = "label7";
+            label7.Size = new Size(51, 24);
+            label7.TabIndex = 22;
+            label7.Text = "Price";
+            label7.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // QtyTb
+            // 
+            QtyTb.Location = new Point(545, 117);
+            QtyTb.Name = "QtyTb";
+            QtyTb.Size = new Size(180, 38);
+            QtyTb.TabIndex = 19;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Calibri", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.Black;
+            label5.Location = new Point(545, 86);
+            label5.Name = "label5";
+            label5.Size = new Size(40, 24);
+            label5.TabIndex = 18;
+            label5.Text = "Qty";
+            label5.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // BCatCb
+            // 
+            BCatCb.Font = new Font("Calibri", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            BCatCb.ForeColor = SystemColors.WindowFrame;
+            BCatCb.FormattingEnabled = true;
+            BCatCb.Items.AddRange(new object[] { "Programming", "Networkng", "Maths", "Physics", "Bio", "English" });
+            BCatCb.Location = new Point(388, 117);
+            BCatCb.Name = "BCatCb";
+            BCatCb.Size = new Size(151, 26);
+            BCatCb.TabIndex = 17;
+            BCatCb.Text = "Select Category";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Calibri", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Black;
+            label2.Location = new Point(388, 86);
+            label2.Name = "label2";
+            label2.Size = new Size(98, 24);
+            label2.TabIndex = 16;
+            label2.Text = "Categories";
+            label2.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.Teal;
+            button7.Font = new Font("Garamond", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button7.ForeColor = Color.Black;
+            button7.Location = new Point(373, 20);
+            button7.Name = "button7";
+            button7.Size = new Size(193, 45);
+            button7.TabIndex = 15;
+            button7.Text = "Books";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Calibri", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.ForeColor = Color.FromArgb(192, 0, 0);
+            label8.Location = new Point(1028, 0);
+            label8.Name = "label8";
+            label8.Size = new Size(20, 22);
+            label8.TabIndex = 12;
+            label8.Text = "X";
+            label8.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // BAuthorTb
+            // 
+            BAuthorTb.Location = new Point(202, 116);
+            BAuthorTb.Name = "BAuthorTb";
+            BAuthorTb.Size = new Size(180, 38);
+            BAuthorTb.TabIndex = 9;
+            // 
+            // BTitleTb
+            // 
+            BTitleTb.Location = new Point(16, 116);
+            BTitleTb.Name = "BTitleTb";
+            BTitleTb.Size = new Size(180, 38);
+            BTitleTb.TabIndex = 8;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Calibri", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Black;
+            label3.Location = new Point(202, 89);
+            label3.Name = "label3";
+            label3.Size = new Size(69, 24);
+            label3.TabIndex = 7;
+            label3.Text = "Author";
+            label3.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Calibri", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(16, 89);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 24);
+            label1.TabIndex = 6;
+            label1.Text = "Book Title ";
+            label1.TextAlign = ContentAlignment.TopCenter;
             // 
             // Book
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightSkyBlue;
             ClientSize = new Size(1280, 720);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Book";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Book";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Label label9;
+        private PictureBox pictureBox1;
+        private Button button6;
+        private Button button1;
+        private Panel panel2;
+        private Button Refreshbtn;
+        private ComboBox CatCbAearch;
+        private DataGridView dataGridView1;
+        private Button EditBtn;
+        private Button Deletebtn;
+        private Button ResetBtn;
+        private Button Savebtn;
+        private TextBox PriceTb;
+        private Label label7;
+        private TextBox QtyTb;
+        private Label label5;
+        private ComboBox BCatCb;
+        private Label label2;
+        private Button button7;
+        private Label label8;
+        private TextBox BAuthorTb;
+        private TextBox BTitleTb;
+        private Label label3;
+        private Label label1;
+        private Label label4;
     }
 }
