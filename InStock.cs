e74969672360c803c226586fc3d92419a3c61ce3
Yasinsa Mahanama <yasinsamahanama@gmail.com>
2024-05-28 06:07:34 +0530

    In Stock page updated

--- InStock.cs
+++ InStock.cs
@@ -16,5 +16,77 @@ namespace OnlineBookShop
         {
             InitializeComponent();
         }
+
+        private void BookBtnI_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the Book form
+            Book bookForm = new Book();
+
+            // Show the Book form
+            bookForm.Show();
+
+            // Hide the current In Stock form
+            this.Hide();
+        }
+
+        private void UserBtnI_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the User form
+            User userForm = new User();
+
+            // Show the User form
+            userForm.Show();
+
+            // Hide the current In Stock form
+            this.Hide();
+        }
+
+        private void InStockBtnI_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the In Stock form
+            InStock inStockForm = new InStock();
+
+            // Show the In Stock form
+            inStockForm.Show();
+
+            // Hide the current In Stock form
+            this.Hide();
+        }
+
+        private void SaleBtnI_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the Sale form
+            Sale saleForm = new Sale();
+
+            // Show the Sale form
+            saleForm.Show();
+
+            // Hide the current In Stock form
+            this.Hide();
+        }
+
+        private void AboutBtnI_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the About form
+            About aboutForm = new About();
+
+            // Show the About form
+            aboutForm.Show();
+
+            // Hide the current In Stock form
+            this.Hide();
+        }
+
+        private void ContactBtnI_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the Contact form
+            Contact contactForm = new Contact();
+
+            // Show the About form
+            contactForm.Show();
+
+            // Hide the current In Stock form
+            this.Hide();
+        }
     }
 }
